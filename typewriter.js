3bbb8109e0cbdd0972c5306761ca450cb5dcd518
Sheldon Malboeuf <smalboeuf01@gmail.com>
2020-02-10 21:59:23 +0000

    Animation for sentence

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs\n";
+let timer = 0;
+
+for (const char of sentence) {
+
+  setTimeout(() => {
+    process.stdout.write(char);
+  },timer)
+  timer += 50;
+
+}
